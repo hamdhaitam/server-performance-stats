bc2bcd1ec7030f43688aaf9b7133fd6717dcb8f9
hamdhaitam <hamdhaitam@gmail.com>
2025-06-15 10:22:32 +0200

    Update server-stats.sh

--- server-stats.sh
+++ server-stats.sh
@@ -4,65 +4,65 @@ echo -e "Server Performance Stats:"
 echo -e "-------------------------"
 
 # OS version
-os_version=$(grep -E '^(VERSION|NAME)=' /etc/os-release)
+OS_VERSION=$(grep -E '^(VERSION|NAME)=' /etc/os-release)
 echo -e "\nOS version: "
-echo "$os_version"
+echo "$OS_VERSION"
 
 # Uptime
-server_uptime=$(uptime -p)
+SERVER_UPTIME=$(uptime -p)
 echo -e "\nUptime: "
-echo "$server_uptime"
+echo "$SERVER_UPTIME"
 
 # Load average
-load_average=$(cat /proc/loadavg)
+LOAD_AVERAGE=$(cat /proc/loadavg)
 echo -e "\nLoad average: "
-echo "$load_average"
+echo "$LOAD_AVERAGE"
 
 # Logged in users
-logged_users=$(who)
+LOGGED_USERS=$(who)
 echo -e "\nLogged in users: "
-if [ -z "$logged_users" ]; then
+if [ -z "$LOGGED_USERS" ]; then
     echo "No users currently logged in."
 else
-    echo "$logged_users"
+    echo "$LOGGED_USERS"
 fi
 
 # Failed login attempts
-failed_logins=$(journalctl -u sshd -b | grep "Failed password")
+FAILED_LOGINS=$(journalctl -u sshd -b | grep "Failed password")
 echo -e "\nFailed login attempts: "
-if [ -z "$failed_logins" ]; then
+if [ -z "$FAILED_LOGINS" ]; then
     echo "No failed login attempts detected."
 else
-    echo "$failed_logins"
+    echo "$FAILED_LOGINS"
 fi
 
 # Total CPU usage
-total_cpu_usage=$(top -bn2 | grep "Cpu(s)" | tail -n1 | sed 's/.*, *\([0-9.]*\)%* id.*/\1/' | awk '{ printf("%.1f%%", 100 - $1) }')
+TOTAL_CPU_USAGE=$(top -bn2 | grep "Cpu(s)" | tail -n1 | sed 's/.*, *\([0-9.]*\)%* id.*/\1/' | awk '{ printf("%.1f%%", 100 - $1) }')
 echo -e "\nTotal CPU usage: "
-echo "$total_cpu_usage"
+echo "$TOTAL_CPU_USAGE"
 
 # Total memory usage (Free vs Used including percentage)
-total_memory_usage=$(free -m | awk '/Mem:/ {
+TOTAL_MEMORY_USAGE=$(free -m | awk '/Mem:/ {
 total=$2; used=$3; free=$4;
 printf("Total: %dMB (%.1f%% used)\nUsed: %dMB\nFree: %dMB", total, used/total * 100, used, free) 
 }')
 echo -e "\nTotal memory usage: "
-echo "$total_memory_usage"
+echo "$TOTAL_MEMORY_USAGE"
 
 # Total disk usage (Free vs Used including percentage)
-total_disk_usage=$(df -BM / | awk 'NR==2 {
+TOTAL_DISK_USAGE=$(df -BM / | awk 'NR==2 {
 total=$2; used=$3; free=$4;
 printf("Total: %dMB (%.1f%% used)\nUsed: %dMB\nFree: %dMB", total, used/total * 100, used, free)
 }')
 echo -e "\nTotal disk usage: "
-echo "$total_disk_usage"
+echo "$TOTAL_DISK_USAGE"
 
 # Top 5 processes by CPU usage
-top_proc_by_cpu_usage=$(ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | head -n 6)
+TOP_PROC_BY_CPU_USAGE=$(ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | head -n 6)
 echo -e "\nTop 5 processes by CPU usage: "
-echo "$top_proc_by_cpu_usage"
+echo "$TOP_PROC_BY_CPU_USAGE"
 
 # Top 5 processes by memory usage
-top_proc_by_mem_usage=$(ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head -n 6)
+TOP_PROC_BY_MEM_USAGE=$(ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head -n 6)
 echo -e "\nTop 5 processes by memory usage: "
-echo "$top_proc_by_mem_usage"
+echo "$TOP_PROC_BY_MEM_USAGE"
